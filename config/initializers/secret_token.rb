51fffea08a2e2c6f446564e90a40ca55e6bce2da
Chris Bolton <Iknewthisguy@yahoo.com>
2012-05-07 20:40:15 -0500

    Revised app name and included user authentication

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Voluntrme::Application.config.secret_token = '1f05dd0ef452bd989d1550f4b7c1f98cda03b3b2ee796f4af0fc2489be3d5e3c44edc34366171e20d7b56c66e35f3c29a6f9a1a8c24f2fe9af6a939cf29b00ca'
