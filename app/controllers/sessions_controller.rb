11c70218c99237c201ae922369368ed18fc92004
Haseeb <haseeb.javed@gmail.com>
2012-05-23 17:21:18 -0500

    redirect logout to the login page instead of signup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,6 +18,6 @@ class SessionsController < ApplicationController
   
   def destroy
     session[:volunteer] = nil
-    redirect_to root_url, :notice => "Logged out!"
+    redirect_to log_in_url, :notice => "Logged out!"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Voluntrme::Application.routes.draw do
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "log_in" => "sessions#new", :as => "log_in"
+  post "log_in" => "sessions#create", :as => "create"
   root :to => "volunteers#new"
   resources :volunteers, only: [:new, :create, :show, :destroy]
 end
