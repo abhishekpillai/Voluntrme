818a2e6c34711056dce0250cd8b72948349a0850
Daniel Kaplan <daniel.scott.kaplan@gmail.com>
2012-05-23 16:44:58 -0500

    Created Form For N New Volunteer

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -1,6 +1,14 @@
 class VolunteersController < ApplicationController
   
   def new
+    @volunteer = Volunteer.new
+  end
+  
+  def create 
+    @volunteer = Volunteer.create(params[:volunteer])
+      session[:volunteer] = @volunteer.id
+    
+    redirect_to root_url, notice: "You are now a Volunteer!"
   end
   
 end
