af71e77bb6a40a6d35cfb757b9e5125714f34717
Daniel Kaplan <daniel.scott.kaplan@gmail.com>
2012-05-23 17:49:37 -0500

    fixed header aand now root_to log in page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Voluntrme::Application.routes.draw do
-  get "log_out" => "sessions#destroy", :as => "log_out"
+
+  resources :volunteers
+  
+  root :to => "sessions#new"
+  
   get "log_in" => "sessions#new", :as => "log_in"
   post "log_in" => "sessions#create", :as => "create"
-  root :to => "volunteers#new"
-  resources :volunteers, only: [:new, :create, :show, :destroy]
+  
+  get "log_out" => "sessions#destroy", :as => "log_out"    
+  
 end
