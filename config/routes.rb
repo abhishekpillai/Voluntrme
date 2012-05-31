4362bcac74d2e47d647eaf14841110d6588c6d37
Haseeb <haseeb.javed@gmail.com>
2012-05-31 17:39:21 -0500

    updated routes and layout

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Voluntrme::Application.routes.draw do
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "log_in" => "sessions#new", :as => "log_in"
   post "log_in" => "sessions#create", :as => "create"
-  root :to => "volunteers#new"
-  resources :volunteers, only: [:new, :create, :show, :destroy]
+  root :to => "sessions#new"
+  resources :volunteers
 end
